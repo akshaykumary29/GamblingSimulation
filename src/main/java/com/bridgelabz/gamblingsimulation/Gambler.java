6c4a870452bc63ec33fe718e38de84f2d9d85a49
akshay <akshay.yamgar29@gmail.com>
2021-10-20 08:51:36 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bridgelabz/gamblingsimulation/Gambler.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblingsimulation;
+
+public class Gambler {
+    public static void main(String[] args) {
+        System.out.println("Welcome To the Gambler Simulation Program");
+    }
+}
